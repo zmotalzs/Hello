bf4235fd2d8bb6fcf52553a6f18566905eacb244
zmotalzs <75839438+zmotalzs@users.noreply.github.com>
2020-12-19 21:13:29 +0800

    cc

--- motor.c
+++ motor.c
@@ -1,14 +1,14 @@
 #include "stm32l1xx.h"
 #include "stm32l1xx_ll_system.h"
 #include "stm32l1xx_ll_bus.h"
-#include "stm32l1xx_ll_gpio.h"
-#include "stm32l1xx_ll_pwr.h"
-#include "stm32l1xx_ll_rcc.h"
 #include "stm32l1xx_ll_utils.h"
+#include "stm32l1xx_ll_rcc.h"
+#include "stm32l1xx_ll_pwr.h"
+#include "stm32l1xx_ll_gpio.h"
+#include "stm32l1xx_ll_usart.h"
+#include "stm32l1xx_ll_lcd.h"
+#include "stm32l152_glass_lcd.h"
 #include "stm32l1xx_ll_tim.h"
-
-#define TIMx_PSC			3200 
-#define TIMx_ARR			100
 
 //7segment
 #define Num_0	(uint32_t) LL_GPIO_PIN_2  | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 
@@ -20,33 +20,30 @@
 #define Num_6	(uint32_t) LL_GPIO_PIN_2  | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15 
 #define Num_7	(uint32_t) LL_GPIO_PIN_2  | LL_GPIO_PIN_10 | LL_GPIO_PIN_11  
 #define Num_8	(uint32_t) LL_GPIO_PIN_2  | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15 
-#define Num_9	(uint32_t) LL_GPIO_PIN_2  | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15 
-
-uint8_t i;
-uint32_t seg0percent[4] = {Num_0,	Num_0, Num_0, Num_0};
-uint32_t seg20percent[4] = {Num_0,	Num_0, Num_2, Num_0};	
-uint32_t seg40percent[4] = {Num_0,	Num_0, Num_4, Num_0};
-uint32_t seg60percent[4] = {Num_0,	Num_0, Num_6, Num_0};
-uint32_t seg80percent[4] = {Num_0,	Num_0, Num_8, Num_0};
-uint32_t seg100percent[4] = {Num_0,	Num_1, Num_0, Num_0};
-
-uint32_t digit[4] = { LL_GPIO_PIN_0, LL_GPIO_PIN_1, LL_GPIO_PIN_2, LL_GPIO_PIN_3 };
+#define Num_9	(uint32_t) LL_GPIO_PIN_2  | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_14 | LL_GPIO_PIN_155 
 
 
-uint16_t x = 0;
+
+/*for 10ms update event*/
+#define TIMx_PSC			3200 
+#define TIMx_ARR			100
+
+float x = 100;
+uint8_t i = 0 ;
 
 void SystemClock_Config(void);
 void TIM_BASE_Config(void);
+void TIM_OC_GPIO_Config(void);
 void TIM_OC_Config(void);
-void GPIO_Config(void);
-void EXTI_Config(void);
+void EXTI_Config();
+void GPIO_Config();
+void SevenSegment_Display();
 
-int main()
+int main(void)
 {
-	SystemClock_Config();
-	GPIO_Config();
+  SystemClock_Config();
 	TIM_BASE_Config();
-	EXTI_Config();
+	GPIO_Config();
 	
 	LL_GPIO_SetOutputPin(GPIOB,LL_GPIO_PIN_7);
 	LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_4);
@@ -54,106 +51,10 @@ int main()
 	while(1)
 	{
 		TIM_OC_Config();
+		SevenSegment_Display();
 	}
-
 }
-
-void SevenSegment_Display()
-{
-	
-	if (x == 0)
-		{
-			for(i=0;i<4;i++){
-				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
-        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
-				LL_GPIO_SetOutputPin(GPIOB, seg0percent[i]);
-				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
-				LL_mDelay(1);
-			}
-		}
-		else if (x == 20)
-		{
-			for(i=0;i<4;i++){
-				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
-        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
-				LL_GPIO_SetOutputPin(GPIOB, seg20percent[i]);
-				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
-				LL_mDelay(1);
-			}
-		}
-		else if (x == 40)
-		{
-			for(i=0;i<4;i++){
-				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
-        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
-				LL_GPIO_SetOutputPin(GPIOB, seg40percent[i]);
-				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
-				LL_mDelay(1);
-			}
-		}
-		else if (x == 60)
-		{
-			for(i=0;i<4;i++){
-				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
-        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
-				LL_GPIO_SetOutputPin(GPIOB, seg60percent[i]);
-				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
-				LL_mDelay(1);
-			}
-			
-		}
-		else if (x == 80)
-		{
-			for(i=0;i<4;i++){
-				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
-        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
-				LL_GPIO_SetOutputPin(GPIOB, seg80percent[i]);
-				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
-				LL_mDelay(1);
-			}	
-			
-		}
-		else if (x == 100)
-		{
-			for(i=0;i<4;i++){
-				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
-        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
-				LL_GPIO_SetOutputPin(GPIOB, seg100percent[i]);
-				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
-				LL_mDelay(1);
-			}
-		}
-}
-
-void EXTI0_IRQHandler(void)
-{
-		EXTI->PR |= (1<<0);
-		
-		if(LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_0) == RESET) //Press OFF Switch
-		{
-			if (x >= 100)
-				x = 0;
-			else
-				x = x+20;
-		}	
- 
-}
-
-void EXTI_Config()
-{
-		 RCC->APB2ENR |= (1<<0);								// Enable EXTICR2 (EXITCH Line0)
-
-     SYSCFG->EXTICR[0] &= ~(15<<0);					// EXTICR1 :	EXTI0 enable to PA0
-		 EXTI->IMR  |= (1<<0);      						// enable Interrupt masking registor to EXTI0 and EXTI1
-     EXTI->FTSR |= (1<<0); 									// enable Falling edge at EXTI0 and EXTI1
-			
-     //Enable IRQ
-     NVIC_EnableIRQ((IRQn_Type)6);
-     NVIC_SetPriority((IRQn_Type)6,0);
-
-}
-
-void GPIO_Config(void)
+void GPIO_Config()
 {
 	LL_GPIO_InitTypeDef l239d;
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
@@ -187,6 +88,136 @@ void GPIO_Config(void)
 
   Itc4727_initstruct.Pin = LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 ;
   LL_GPIO_Init(GPIOC , &Itc4727_initstruct);
+	EXTI_Config();
+	
+}
+void SevenSegment_Display()
+{
+	
+	if (x == 0)
+		{
+			LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
+      LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
+			LL_GPIO_SetOutputPin(GPIOB, Num_0);
+			LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_2);
+		}
+		else if (x == 20)
+		{
+			uint32_t segment[2] = { Num_2,Num_0};
+			uint32_t digit[2] = { LL_GPIO_PIN_1,LL_GPIO_PIN_2};
+			for(i=0;i<2;i++){
+				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
+        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
+				LL_GPIO_SetOutputPin(GPIOB, segment[i]);
+				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
+				LL_mDelay(1);
+			}
+		}
+		else if (x == 40)
+		{
+			uint32_t segment[2] = { Num_4,Num_0};
+			uint32_t digit[2] = { LL_GPIO_PIN_1,LL_GPIO_PIN_2};
+			for(i=0;i<2;i++){
+				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
+        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
+				LL_GPIO_SetOutputPin(GPIOB, segment[i]);
+				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
+				LL_mDelay(1);
+			}
+		}
+		else if (x == 60)
+		{
+			uint32_t segment[2] = { Num_6,Num_0};
+			uint32_t digit[2] = { LL_GPIO_PIN_1,LL_GPIO_PIN_2};
+			for(i=0;i<2;i++){
+				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
+        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
+				LL_GPIO_SetOutputPin(GPIOB, segment[i]);
+				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
+				LL_mDelay(1);
+			}
+			
+		}
+		else if (x == 80)
+		{
+			uint32_t segment[2] = { Num_8,Num_0};
+			uint32_t digit[2] = { LL_GPIO_PIN_1,LL_GPIO_PIN_2};
+			for(i=0;i<2;i++){
+				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
+        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
+				LL_GPIO_SetOutputPin(GPIOB, segment[i]);
+				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
+				LL_mDelay(1);
+			}	
+			
+		}
+		else if (x == 100)
+		{
+			uint32_t segment[3] = { Num_1,Num_0,Num_0};
+			uint32_t digit[3] = { LL_GPIO_PIN_0,LL_GPIO_PIN_1,LL_GPIO_PIN_2};
+			for(i=0;i<3;i++){
+				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
+        LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15); 
+				LL_GPIO_SetOutputPin(GPIOB, segment[i]);
+				LL_GPIO_SetOutputPin(GPIOC, digit[i]);
+				LL_mDelay(1);
+			}
+		}
+}
+void EXTI0_IRQHandler(void)
+{
+		EXTI->PR |= (1<<0);
+		
+		if(LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_0) == RESET) //Press OFF Switch
+		{
+			if (x == 0)
+				x = 100;
+			else
+				x = x-20;
+		}	
+
+}
+
+void EXTI_Config()
+{
+		 RCC->APB2ENR |= (1<<0);								// Enable EXTICR2 (EXITCH Line0)
+
+     SYSCFG->EXTICR[0] &= ~(15<<0);					// EXTICR1 :	EXTI0 enable to PA0
+		 EXTI->IMR  |= (1<<0);      						// enable Interrupt masking registor to EXTI0 and EXTI1
+     EXTI->FTSR |= (1<<0); 									// enable Falling edge at EXTI0 and EXTI1
+			
+     //Enable IRQ
+     NVIC_EnableIRQ((IRQn_Type)6);
+     NVIC_SetPriority((IRQn_Type)6,0);
+
+}
+
+void TIM3_IRQHandler(void)
+{
+	if(LL_TIM_IsActiveFlag_CC2(TIM3) == SET)
+	{
+		LL_TIM_ClearFlag_CC2(TIM3);
+	}
+}
+
+void TIM_OC_Config(void)
+{
+	LL_TIM_OC_InitTypeDef tim_oc_initstructure;
+	
+	tim_oc_initstructure.OCState 				= LL_TIM_OCSTATE_DISABLE;
+	tim_oc_initstructure.OCMode 				= LL_TIM_OCMODE_PWM1;
+	tim_oc_initstructure.OCPolarity 		= LL_TIM_OCPOLARITY_HIGH;
+	tim_oc_initstructure.CompareValue 	= x; 
+	LL_TIM_OC_Init(TIM3, LL_TIM_CHANNEL_CH2, &tim_oc_initstructure);
+	
+	/*Interrupt Configure*/
+	NVIC_SetPriority(TIM3_IRQn, 29);
+	NVIC_EnableIRQ(TIM3_IRQn);
+	LL_TIM_EnableIT_CC2(TIM3);
+	
+	/*Start Output Compare in PWM Mode*/
+	LL_TIM_CC_EnableChannel(TIM3, LL_TIM_CHANNEL_CH2);
+	LL_TIM_EnableCounter(TIM3);
 }
 
 void TIM_BASE_Config(void)
@@ -200,39 +231,37 @@ void TIM_BASE_Config(void)
 	timbase_initstructure.Autoreload = TIMx_ARR - 1;
 	timbase_initstructure.Prescaler =  TIMx_PSC- 1;
 	
-	LL_TIM_Init(TIM3, &timbase_initstructure);
+	LL_TIM_Init(TIM4, &timbase_initstructure);
 
 }
 
-void TIM_OC_Config(void)
+void TIM_OC_GPIO_Config(void)
 {
-	LL_TIM_OC_InitTypeDef tim_oc_initstructure;
+	LL_GPIO_InitTypeDef gpio_initstructure;
 	
-	TIM_BASE_Config();
-
-	tim_oc_initstructure.OCState = LL_TIM_OCSTATE_DISABLE;
-	tim_oc_initstructure.OCMode = LL_TIM_OCMODE_PWM1;
-	tim_oc_initstructure.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
-	tim_oc_initstructure.CompareValue = x; 
-	LL_TIM_OC_Init(TIM3, LL_TIM_CHANNEL_CH2, &tim_oc_initstructure);
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
 	
-	/*Interrupt Configure*/
-	NVIC_SetPriority(TIM3_IRQn, 29);
-	NVIC_EnableIRQ(TIM3_IRQn);
-	LL_TIM_EnableIT_CC2(TIM3);
+	//Fq O/P
+	gpio_initstructure.Mode = LL_GPIO_MODE_ALTERNATE;
+	gpio_initstructure.Alternate = LL_GPIO_AF_2;
+	gpio_initstructure.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	gpio_initstructure.Pin = LL_GPIO_PIN_6;
+	gpio_initstructure.Pull = LL_GPIO_PULL_NO;
+	gpio_initstructure.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+	LL_GPIO_Init(GPIOB, &gpio_initstructure);
 	
-	/*Start Output Compare in PWM Mode*/
-	LL_TIM_CC_EnableChannel(TIM3, LL_TIM_CHANNEL_CH2);
-	LL_TIM_EnableCounter(TIM3);
+	//User Button
+	gpio_initstructure.Mode				= LL_GPIO_MODE_INPUT;
+	gpio_initstructure.Pin 				= LL_GPIO_PIN_0;
+	gpio_initstructure.OutputType	= LL_GPIO_OUTPUT_PUSHPULL;
+	gpio_initstructure.Pull				= LL_GPIO_PULL_NO;
+	gpio_initstructure.Speed				= LL_GPIO_SPEED_FREQ_HIGH;
+	LL_GPIO_Init(GPIOA, &gpio_initstructure);
+	EXTI_Config();
+		
 }
 
-void TIM3_IRQHandler(void)
-{
-	if(LL_TIM_IsActiveFlag_CC2(TIM3) == SET)
-	{
-		LL_TIM_ClearFlag_CC2(TIM3);
-	}
-}
 
 void SystemClock_Config(void)
 {
@@ -287,4 +316,6 @@ void SystemClock_Config(void)
   /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
   LL_SetSystemCoreClock(32000000);
 }
+	
+	
 
